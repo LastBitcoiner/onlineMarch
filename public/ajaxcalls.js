5136fc28405d4e53cff3ee4e40ba675975190bc5
SpectreSh <code.omidsha@gmail.com>
2020-10-30 15:28:50 +0100

    fix callback
    need to create branch and fix and create maintainable counter and rename ajax calls

--- public/ajaxcalls.js
+++ public/ajaxcalls.js
@@ -1,4 +1,4 @@
-function SloganCount(SloganNumber) {
+/* function SloganCount(SloganNumber) {
     const request = new XMLHttpRequest();
     request.onreadystatechange = function () {
         if (request.readyState == 4) {
@@ -24,3 +24,46 @@ function SubmitSlogan(SloganNumber) {
     console.log(foo);
     document.getElementById("2").innerHTML = `down with America ${foo}`;
 }
+*/
+
+function declareText(SloganNumber) {
+    document.getElementById("2").innerHTML = `مرگ بر آمریکا ${SloganNumber}`;
+}
+
+function pushSlogan(SloganNumber, callback) {
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            console.log(`مرگ بر امریکا ${xhr.responseText}`);
+            callback(xhr.responseText);
+            //document.getElementById("2").innerHTML = `down with America ${xhr.responseTextfoo}`;
+        }
+    }
+    xhr.open('GET', `/Slogans/${SloganNumber}`, true);
+    xhr.send();
+}
+
+function checkSlogan(SloganNumber, callback) {
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            console.log(`checked number: ${xhr.responseText}`);
+            callback(xhr.responseText);
+            //document.getElementById("2").innerHTML = `down with America ${xhr.responseTextfoo}`;
+        }
+    }
+    xhr.open('GET', `/${SloganNumber}`, true);
+    xhr.send();
+}
+
+function updateButton(response) {
+    console.log(response);
+    document.getElementById("2").innerHTML = `مرگ بر آمریکا ${response}`;
+}
+
+checkSlogan(2,declareText);
+/*
+function sendSlogan(SloganNumber) {
+    pushSlogan(SloganNumber, mycallback);
+}
+*/
