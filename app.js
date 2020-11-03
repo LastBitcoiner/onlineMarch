28f4ef72e23fb9233c017a8260fac4dcfa73c651
SpectreSh <code.omidsha@gmail.com>
2020-11-03 09:45:48 +0100

    Update app.js

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ const path = require("path");
 
 const app = express();
 
-let visitCount = 115000;
+let visitCount = 155000;
 let Slogans = new Array(6).fill(0);
-Slogans = [660000,510000,295900,300000,365000];
+Slogans = [750000,565000,350900,350000,395000];
 
 /**
  *  App Configuration */
